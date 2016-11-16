0860225cc0a2022d1dc3071603244b186049f1ac
Sin-Yao <andychao@AndydeMacBook-Air.local>
2016-11-16 18:33:58 +0800

    js

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var margin = {top: 50, right: 50, bottom: 150, left: 100},
     width = 1024 - margin.left - margin.right,
     height = 768 - margin.top - margin.bottom;
 
-var ctrl = d3.select("body").append("svg").attr("width", width).attr("height", height);
+var ctrl = d3.select("content").append("svg").attr("width", width).attr("height", height);
 d3.csv(" https://sin-yao.github.io/D3/np01.csv", 
 	function(data)
 	{
